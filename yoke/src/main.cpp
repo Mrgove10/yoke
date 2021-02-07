7cc190cb6f6714601096d654fdb3f4a3df28099d
Adrien RICHARD <richardadrien0@gmail.com>
2021-02-07 15:56:24 +0100

    added code

--- yoke/src/main.cpp
+++ yoke/src/main.cpp
@@ -1,22 +1,58 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1327.h>
 
-int lol;
-int potar = A0;
-int relay = D0;
+// Axis
+#define PUTCH A0
+#define ROLL A1
+// Joystick
+#define JOY_Y A2
+#define JOY_X A3
+#define JOY_SWITCH 15
+// Right Switchs
+#define TOP_SWITCH 0
+#define BOT_SWITCH 1
+// Center Switchs
+#define GEAR_SWITCH 7
+#define BATTERY_SWITCH 8
+#define AUTO_PILOT_SWITCH 9
+#define SPEED A10;
+#define FLAP_UP_SWITCH 14
+#define FLAP_DOWN_SWITCH 16
+#define ENCODER_CLOCK 4
+#define ENCODER_COUNTER_CLOCK 5
+#define ENCODER_SWITCH 6
+//Lcd
+//https://learn.adafruit.com/adafruit-grayscale-1-5-128x128-oled-display?view=all
+//https://github.com/adafruit/Adafruit_SSD1327/blob/master/examples/ssd1327_test/ssd1327_test.ino
+#define OLED_RESET -1
+Adafruit_SSD1327 display(128, 128, &Wire, OLED_RESET, 1000000);
 
 void setup()
 {
-
-  pinMode(potar, INPUT);
-  pinMode(relay, OUTPUT);
   Serial.begin(115200);
+  Serial.println("SSD1327 OLED test");
+
+  if (!display.begin(0x78))
+  {
+    Serial.println("Unable to initialize OLED");
+    while (1)
+      yield();
+  }
+  display.clearDisplay();
+  display.display();
+
+  //pinMode(potar, INPUT);
+  //pinMode(relay, OUTPUT);
 }
 
-void loop() // run over and over
+void loop()
 {
+  /*
   lol = analogRead(potar);
   Serial.println(lol);
   digitalWrite(relay, HIGH);
   delay(lol);
   digitalWrite(relay, LOW);
+  */
 }
