5bbada71f04426bd6bd06101a53dd8ce4b6322a1
Adrien RICHARD <richardadrien0@gmail.com>
2021-02-14 23:35:11 +0100

    typescript

---
+++ soft/src/data.ts
@@ -0,0 +1,13 @@
+class Data {
+    greeting: string;
+  
+    constructor(message: string) {
+      this.greeting = message;
+    }
+  
+    greet() {
+      return "Hello, " + this.greeting;
+    }
+  }
+  
+  let greeter = new Greeter("world");
